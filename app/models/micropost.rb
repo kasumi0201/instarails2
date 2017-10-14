6d2612532525574166949bb211e5a46a0deb4619
kasumi <kasumi0201@gmail.com>
2017-10-15 01:10:00 +1100

    set up

---
+++ app/models/micropost.rb
@@ -0,0 +1,7 @@
+class Micropost < ApplicationRecord
+  belongs_to :user
+    # マイクロポストの最大文字数を140文字に制限する。
+  validates :content, length: { maximum: 140 }
+  # マイクロポストのコンテンツが存在しているかどうかの確認
+     presence: true
+end
